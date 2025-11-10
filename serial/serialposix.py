5fb3bc6b52376289990f47fb4f293a0332363d31
zsquareplusc <cliechti@gmx.net>
2025-11-10 02:26:44 +0100

    feat(serialposix): update mips detection

--- serial/serialposix.py
+++ serial/serialposix.py
@@ -81,7 +81,7 @@ if plat[:5] == 'linux':    # Linux (confirmed)  # noqa
     CMSPAR = 0o10000000000  # Use "stick" (mark/space) parity
 
     # baudrate ioctls
-    if "mips" in platform.machine().lower():
+    if platform.machine().startswith('mips'):
         TCGETS2 = 0x4030542A
         TCSETS2 = 0x8030542B
         BAUDRATE_OFFSET = 10
